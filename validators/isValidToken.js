b70fe2fd80944455a9ac8329ccb198dcb3e2bde8
Leonardo Balbastro <leonardonbalbastro@gmail.com>
2024-06-08 18:07:46 -0300

    modificacion de isValidToken

--- validators/isValidToken.js
+++ validators/isValidToken.js
@@ -1,9 +1,7 @@
 const jwt = require('jsonwebtoken')
 const isValidToken = (req, res) => {
-
     try {
         const token = req.header('TokenJWT')
-        // console.log(jwt.verify(token, process.env.SECRET_KEY))
         const isValid = jwt.verify(token, process.env.SECRET_KEY)
         res.send(true)
     } catch (error) {
